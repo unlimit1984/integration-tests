123f5f53f8fdc41c8999c05ea1267425ca8c383a
unlimit1984 <unlimit_rmc@mail.ru>
2020-05-19 20:06:21 +0200

    [integration tests] Add class checking test

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,5 +1,6 @@
 import { VoterComponent } from './voter.component';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 
 describe('VoterComponent', () => {
 
@@ -15,6 +16,29 @@ describe('VoterComponent', () => {
     // fixture.debugElement // wrapper on fixture.nativeElement
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    // fixture.debugElement.query(By.directive(VoterComponent)).
+    // fixture.debugElement.queryAll(By.css('.vote-count'))
+    let de = fixture.debugElement.query(By.css('.vote-count')); // de -> Debug Element
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain('21');
+  });
+
+  it('should highlight the upvote button if I have upvoted', () => {
+    component.myVote =1;
+
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    // let el: HTMLElement = de.nativeElement;
+    // expect(el.className).toContain('highlighted');
+
+    expect(de.classes['highlighted']).toBeTruthy();
   });
 });
