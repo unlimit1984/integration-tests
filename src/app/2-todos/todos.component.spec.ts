1c0534263afba61682d66f5495b590e174f46316
unlimit1984 <unlimit_rmc@mail.ru>
2020-05-19 23:06:01 +0200

    [integration tests] Add spyOn for service and test ngOnInit()

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -4,6 +4,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodosComponent } from './todos.component';
 import { TodoService } from "./todo.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { from } from "rxjs";
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -20,10 +21,19 @@ describe('TodosComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
-    // fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load todos from the server', () => {
+    let service = TestBed.get(TodoService);
+    // let service = fixture.debugElement.injector.get(TodoService);
+
+    spyOn(service, 'getTodos').and.returnValue(from([[1,2,3]]));
+    fixture.detectChanges();
+
+    expect(component.todos.length).toBe(3);
   });
 });
